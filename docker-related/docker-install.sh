f4f23cfd1728dd61eb1751d00a8ac245dc5583aa
humstarman <lueyoung7@gmail.com>
2018-04-28 09:16:38 +0800

    update: delete redundancy

--- docker-related/docker-install.sh
+++ docker-related/docker-install.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-DistributorID=$(lsb_release -i | awk -F ' ' '{print $3}' | tail -n 1)
-
-echo $DistributorID
-MATCHED="YES"
-
-IF0=$(cat /etc/rc.local | grep "sleep 60 && \/sbin\/iptables -P FORWARD ACCEPT")
-
-if [ "CentOS" == "$DistributorID" ]; then
-  cd /etc/yum.repos.d && \
-  if [ -f CentOS-Base.repo ]; then mv -f CentOS-Base.repo CentOS-Base.repo.bak; fi && \
-  wget -O CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo && \
-  yum clean all && \
-  yum makecache && \
-  cd -
+if [ -x "$(command -v yum)" ]; then
+  yum makecache
   yum install -y yum-utils \
     device-mapper-persistent-data \
     lvm2
@@ -22,25 +10,8 @@ if [ "CentOS" == "$DistributorID" ]; then
     https://download.docker.com/linux/centos/docker-ce.repo
   yum install -y docker-ce
   yum install -y conntrack
-  
-  # rc.local
-  if [ -z "$IF0" ]; then
-    echo "sleep 60 && /sbin/iptables -P FORWARD ACCEPT" >> /etc/rc.d/rc.local
-    chmod +x /etc/rc.d/rc.local
-  fi
-elif [ "Ubuntu" == "$DistributorID" ]; then
-  Codename=$( (lsb_release -a) | awk '{print $2}' | tail -n 1 )
-  echo "\
-    deb http://mirrors.aliyun.com/ubuntu/ $Codename main multiverse restricted universe
-    deb http://mirrors.aliyun.com/ubuntu/ $Codename-backports main multiverse restricted universe
-    deb http://mirrors.aliyun.com/ubuntu/ $Codename-proposed main multiverse restricted universe
-    deb http://mirrors.aliyun.com/ubuntu/ $Codename-security main multiverse restricted universe
-    deb http://mirrors.aliyun.com/ubuntu/ $Codename-updates main multiverse restricted universe
-    deb-src http://mirrors.aliyun.com/ubuntu/ $Codename main multiverse restricted universe
-    deb-src http://mirrors.aliyun.com/ubuntu/ $Codename-backports main multiverse restricted universe
-    deb-src http://mirrors.aliyun.com/ubuntu/ $Codename-proposed main multiverse restricted universe
-    deb-src http://mirrors.aliyun.com/ubuntu/ $Codename-security main multiverse restricted universe
-    deb-src http://mirrors.aliyun.com/ubuntu/ $Codename-updates main multiverse restricted universe " > /etc/apt/sources.list
+  FIREWALL="firewalld"
+elif [ -x "$(command -v apt-get)" ]; then
   apt-get update
   apt-get install -y \
     apt-transport-https \
@@ -55,15 +26,14 @@ elif [ "Ubuntu" == "$DistributorID" ]; then
   apt-get update
   apt-get install -y docker-ce
   apt-get install -y conntrack
-  
-  # rc.local
-  if [ -z "$IF0" ]; then
-    sed -i "/exit 0/i\sleep 60 && /sbin/iptables -P FORWARD ACCEPT" /etc/rc.local
-  fi
+  FIREWALL="ufw"
 else
   echo "$(date) - $0 - [ERROR] - unknown Distributor ID."
   exit 1
 fi
+
+systemctl stop $FIREWALL
+systemctl disable $FIREWALL
 
 iptables -P FORWARD ACCEPT
 iptables -F && iptables -X && iptables -F -t nat && iptables -X -t nat
