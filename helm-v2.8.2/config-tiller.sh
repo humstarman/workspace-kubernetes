0263e05e0cf325526bf2b72e0d7531c19a591cfc
humstarman <lueyoung7@gmail.com>
2018-04-28 09:40:58 +0800

    update: add completion bash of helm

--- helm-v2.8.2/config-tiller.sh
+++ helm-v2.8.2/config-tiller.sh
@@ -27,3 +27,8 @@ else
     sed -i '/export HELM_HOST=/d' /etc/profile && echo "export HELM_HOST=$HELM_HOST" >> /etc/profile 
   fi
 fi
+
+if [ -z "$(cat /etc/profile | grep 'source <(helm completion bash)')" ]; then
+  echo 'source <(kubectl completion bash)' >> /etc/profile
+fi
+
