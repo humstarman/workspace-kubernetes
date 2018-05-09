e402f2ebba6193625f3ad91cd31139a93d9e5c57
humstarman <lueyoung7@gmail.com>
2018-05-09 08:43:02 +0800

    update: add the checking process for images

--- kubefolw/pull-and-tag.sh
+++ kubefolw/pull-and-tag.sh
@@ -32,14 +32,24 @@ gcr.io/kubeflow/tensorflow-notebook-cpu"
 for GO_IMAGE in $GO_IMAGES; do
   NAME=${GO_IMAGE##*/}
   #echo $NAME
-  docker pull ${DOCKER_HUB}/$NAME
-  echo "$(date) - [INFO] - image ${DOCKER_HUB}/$NAME pulled."
-  docker tag ${DOCKER_HUB}/$NAME ${LOCAL_REPO}/$NAME
-  echo "$(date) - [INFO] - rename ${DOCKER_HUB}/$NAME as ${LOCAL_REPO}/$NAME."
+  PULLABLE=${DOCKER_HUB}/$NAME 
+  REPOSITORY=${PULLABLE%%:*}
+  TAG=${PULLABLE##*:}
+  [ -z "$TAG" ] && TAG="latest"
+  if [ -n "$(docker images | grep $REPOSITORY | grep $TAG)" ]; then
+    echo "$(date) - [WARN] - $IMAGE already existed."
+  else
+    docker pull $PULLABLE
+    echo "$(date) - [INFO] - image $PULLABLE pulled."
+  fi
+  docker tag $PULLABLE ${LOCAL_REPO}/$NAME
+  echo "$(date) - [INFO] - rename $PULLABLE as ${LOCAL_REPO}/$NAME."
   docker push ${LOCAL_REPO}/$NAME
   echo "$(date) - [INFO] - image ${LOCAL_REPO}/$NAME pushed."
   ansible all -m shell -a "docker pull ${LOCAL_REPO}/$NAME"
   echo "$(date) - [INFO] - image ${LOCAL_REPO}/$NAME pulled at all nodes."
   ansible all -m shell -a "docker tag ${LOCAL_REPO}/$NAME $GO_IMAGE"
   echo "$(date) - [INFO] - rename image ${LOCAL_REPO}/$NAME as ${GO_IMAGE} at all nodes."
+  ansible all -m shell -a "docker rmi ${LOCAL_REPO}/$NAME"
+  echo "$(date) - [INFO] - delete temporary image ${LOCAL_REPO}/$NAME at all nodes."
 done
