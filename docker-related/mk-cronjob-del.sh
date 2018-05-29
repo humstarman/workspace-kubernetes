199f7691b445006beffd9cfaf29f0a8afaacdd75
humstarman <lueyoung7@gmail.com>
2018-05-29 17:20:04 +0800

    update: fix typo

--- docker-related/mk-cronjob-del.sh
+++ docker-related/mk-cronjob-del.sh
@@ -30,14 +30,16 @@ fi
 EOF
 chmod +x /usr/local/bin/del-exited-containers.sh
 
-CRON=/etc/creontab
+CRON=/etc/crontab
 if [ -z "$(cat $CRON | grep del-useless-images.sh)" ]; then
   cat >> $CRON << EOF
+
 2 2 * * * root ansible all -m script -a /usr/local/bin/del-useless-images.sh
 EOF
 fi
 if [ -z "$(cat $CRON | grep del-exited-containers.sh)" ]; then
   cat >> $CRON << EOF
+
 22 2 * * * root ansible all -m script -a /usr/local/bin/del-exited-containers.sh
 EOF
 fi
